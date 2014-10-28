af614f4a21d4644ff5841f6b38cf899537a0cffa
WinKarbik <karbainovs@mail.ru>
2014-10-28 17:34:59 +0800

    update sources

--- common.mk
+++ common.mk
@@ -141,15 +141,21 @@ PRODUCT_COPY_FILES += \
     device/samsung/bcm21553-common/prebuilt/etc/wifi/wpa_supplicant.conf:system/etc/wifi/wpa_supplicant.conf \
     device/samsung/bcm21553-common/prebuilt/bin/get_macaddrs:system/bin/get_macaddrs
 
+# SELinux
+BOARD_SEPOLICY_DIRS += device/samsung/bcm21553-common/sepolicy
+
+BOARD_SEPOLICY_UNION += \
+    file_contexts
+
+# Touchscreen
+PRODUCT_COPY_FILES += \
+    device/samsung/bcm21553-common/prebuilt/usr/idc/sec_touchscreen.idc:system/usr/idc/sec_touchscreen.idc \
+    device/samsung/bcm21553-common/prebuilt/usr/idc/synaptics-rmi-touchscreen.idc:system/usr/idc/synaptics-rmi-touchscreen.idc
+
 # The OpenGL ES API level that is natively supported by this device.
 # This is a 16.16 fixed point number
 PRODUCT_PROPERTY_OVERRIDES := \
     ro.opengles.version=131072
-
-# Dalvik runtime
-PRODUCT_PROPERTY_OVERRIDES += \
- dalvik.vm.jit.codecachesize=0
-
 
 # Graphics properties
 PRODUCT_PROPERTY_OVERRIDES += \
@@ -172,8 +178,15 @@ endif
 
 # Memory
 PRODUCT_PROPERTY_OVERRIDES += \
-    persist.sys.disable_zram=1 \
     ro.config.low_ram=true
+
+# Reduce background apps limit to 12 on low-tier devices
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.sys.fw.bg_apps_limit=12
+
+# Set max background services
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.config.max_starting_bg=6
 
 # Stagefright
 PRODUCT_PROPERTY_OVERRIDES += \
@@ -198,6 +211,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
     wifi.ap.interface=wl0.1 \
     wifi.interface=eth0 \
     wifi.supplicant_scan_interval=60
+
+# SELinux - we're not ready for enforcing mode yet
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.boot.selinux=permissive
 
 # USB / SD card
 PRODUCT_PROPERTY_OVERRIDES += \
